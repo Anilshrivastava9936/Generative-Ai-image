0aa1c7632d45f7189fac1c7a7d6d3ecd5b49816d
Anilshrivastava9936 <136188318+Anilshrivastava9936@users.noreply.github.com>
2025-07-26 17:51:55 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
     // <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen 	bg-gradient-to-b from-teal-50 to-orange-50'>
 
 
-    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen 	bg-gradient-to-b from-orange-400 to-red-500'>
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen 	bg-gradient-to-b from-orange-400 to-red-500 overflow-x-hidden'>
 
 
       <ToastContainer position='bottom-right' />
